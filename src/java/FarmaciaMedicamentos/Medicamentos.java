94139f5bd30ab74ecd5522601989f28168788f03
Unknown <hidalgolucas95@gmail.com>
2018-05-03 22:51:20 -0300

    Pruebas de la url base

--- src/java/FarmaciaMedicamentos/Medicamentos.java
+++ src/java/FarmaciaMedicamentos/Medicamentos.java
@@ -7,6 +7,7 @@ package FarmaciaMedicamentos;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URI;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,15 +35,21 @@ public class Medicamentos extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Medicamentos</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Medicamentos at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+                        
+            StringBuffer url = request.getRequestURL();
+            String uri = request.getRequestURI();
+            String ctx = request.getContextPath();
+            String base = url.substring(0, url.length() - uri.length() + ctx.length()) + "/";
+            
+            URI contextUrl = URI.create(request.getRequestURL().toString())
+                            .resolve(request.getContextPath());
+            out.println(uri);
+            out.println("<br/>");
+            out.println(ctx);
+            out.println("<br/>");
+            out.println(base);
+            out.println("<br/>");
+            out.println(contextUrl);
         }
     }   
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
