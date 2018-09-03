a8c7610e497b1e6b17ad4549d3311fbfa969f4a9
lucasnahuelrieb <lucas.rieb22@gmail.com>
2018-09-03 15:44:13 -0300

    Propagandas terminadas

--- propagandas/Banner propagandas.js
+++ propagandas/Banner propagandas.js
@@ -1,5 +1,9 @@
 
-
+<script>
+window.onload = function() {
+ window.open("http://direccion_de_la_pagina","PopUp","width=ANCHO, height=ALTO, scrollbars=yes, menubar=no, status=no, location=no, resizable=yes");
+}
+</script>
 
 
 
