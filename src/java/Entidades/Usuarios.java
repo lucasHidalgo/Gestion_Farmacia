82923a5b5c647ffaf55c757def0b7677d8e894b4
lucasnahuelrieb <lucas.rieb22@gmail.com>
2018-09-03 12:13:41 -0300

    se van agregando propagandas

--- src/java/Entidades/Usuarios.java
+++ src/java/Entidades/Usuarios.java
@@ -17,15 +17,13 @@ public class Usuarios {
     
      public static boolean ValidarUsuario(Connection conn, String usuario,String clave){
        try{
-            String query = "SELECT * FROM usuarios WHERE us_email = '"+usuario+"' AND clave = '"+clave+"'";
-            //String query = "SELECT * FROM usuarios WHERE us_email = 'lucas@lucas.com' AND clave = '123456'";
-       Statement st = conn.createStatement();
-          
-       ResultSet rs = st.executeQuery(query);
-        if(rs.next()){
-            //obtener todos los campos del usuario, crear un objeto y meterlo en la cookie o lo que sea.
-            return true;
-        }
+            String query = "SELECT * FROM usuarios WHERE us_email = '"+usuario+"' AND clave = '"+clave+"'";     
+            Statement st = conn.createStatement();          
+            ResultSet rs = st.executeQuery(query);
+            if(rs.next()){
+                //obtener todos los campos del usuario, crear un objeto y meterlo en la cookie o lo que sea.
+                return true;
+            }       
        return false;
        }
        catch(Exception ex){
